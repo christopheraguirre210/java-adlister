5e69d19047b0c64bd5779bc9d11cc33a734f3747
Christopher Aguirre <christopher.aguirre210@gmail.com>
2020-01-07 14:11:29 -0600

    hello world servlets

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,28 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+        @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+        public class HelloWorldServlet extends HttpServlet {
 
-    }
-}
+            String name = "";
+
+            @Override
+            protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+                response.setContentType("text/html");
+                PrintWriter out = response.getWriter();
+
+                if(request.getParameter("name") != null){
+                    name = request.getParameter("name");
+                    out.println("<h1>Hello," + name);
+                }else{
+                    out.println("<h1>Hello, World!</h1>");
+                }
+            }
+
+        }
+
