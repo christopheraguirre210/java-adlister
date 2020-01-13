03db5baa4f3e5799da5c66d305c3a1281645954d
Christopher Aguirre <christopher.aguirre210@gmail.com>
2020-01-13 15:28:10 -0600

    sessions and cookies exercise complete

--- src/main/java/LogoutServlet.java
+++ src/main/java/LogoutServlet.java
@@ -5,13 +5,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
-public class ViewProfileServlet extends HttpServlet {
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (request.getSession().getAttribute("user") == null) {
-            response.sendRedirect("/login");
-        } else {
-            request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
-        }
+        request.getSession().invalidate();
+        response.sendRedirect("/login");
     }
 }
