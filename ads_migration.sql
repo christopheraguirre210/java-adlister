71a2e1d4b8d89ccf3b4a5a4b0e77d9ccd5f6a588
Christopher Aguirre <christopher.aguirre210@gmail.com>
2020-01-14 12:00:58 -0600

    jdbc exercise progress

--- ads_migration.sql
+++ ads_migration.sql
@@ -1,6 +1,8 @@
 USE adlister_db;
+DROP TABLE IF EXISTS ads;
+CREATE TABLE ads;
 id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-user_id FOREIGN KEY,
 title VARCHAR(50) NOT NULL,
-description VARCHAR(100)
+description VARCHAR(100),
+FOREIGN KEY(user_id)
 );
---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,30 @@
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+public class MySQLAdsDao implements Ads{
+    private Connection connection;
+
+    public MySQLAdsDao() throws SQLException {
+        Config config = new Config();
+        DriverManager.registerDriver(new Driver());
+        this.connection = DriverManager.getConnection(
+                config.getUrl(),
+                config.getUsername(),
+                config.getPassword()
+        );
+    }
+
+    @Override
+    public List<Ad> all() {
+        return null;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        return null;
+    }
+}
